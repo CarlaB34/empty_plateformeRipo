f32a8b58dc71a2f7d494f1490c5060882084fbd7
Carla <carlita.b34@gmail.com>
2020-12-12 11:33:57 +0100

    postProcess + score

---
+++ Assets/Script/Instanciate/Instantiator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Instantiator : ScriptableObject
+{
+    [SerializeField]
+    private GameObject m_Prefab = null;
+    public void InstantiatePrefab(GameObject p_Prefeab)
+    {
+        Instantiate(p_Prefeab);
+    }
+
+    public void InstantiatePrefab(CollectInfos p_Info)
+    {
+        Instantiate(m_Prefab, p_Info.position, Quaternion.identity);
+    }
+
+    public void InstantiatePrefabInpact(HitInfos p_HitInfo)
+    {
+        Instantiate(m_Prefab, p_HitInfo.impact, Quaternion.identity);
+    }
+
+    
+}
